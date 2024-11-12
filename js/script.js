d2b17e72b7b2ea451d972b2f266a5ebbf7d7633d
YanaStarodoubets <yana.akulenko@gmail.com>
2024-11-12 13:38:35 +0100

    add sort-by, change remove todo

--- js/script.js
+++ js/script.js
@@ -1,80 +1,80 @@
 const inputTitle = document.getElementById("input-title");
 const inputText = document.getElementById("input-text");
 const listContainer = document.getElementById("list-container");
-const addTaskButton = document.getElementById("add-task-button");
-const sortSelect = document.getElementById("sort-by");
+const addTodosButton = document.getElementById("add-todos-button");
+const prioritySelect = document.getElementById("priority");
+const sortBySelect = document.getElementById("sort-by");
+
+document.addEventListener("DOMContentLoaded", getTodos);
+addTodosButton.addEventListener("click", addTodos);
+listContainer.addEventListener("click", clickOnButtons);
+sortBySelect.addEventListener("change", sortSelect);
 
 let toDos = [];
-let editTodo = null;
 
 function getTodos() {
-  toDos = JSON.parse(localStorage.getItem("toDos"));
-  toDos.sort(function (a, b) {
-    if (a.value < b.value) {
-      return -1;
-    }
-    if (a.value > b.value) {
-      return 1;
-    }
-    return 0;
-  });
-  saveTodosInLocalStorage();
-  createTodos();
+  toDos = JSON.parse(localStorage.getItem("toDos")) || [];
+  sortSelect();
+  displayTodos();
 }
 
 document.getElementById("todo__input").addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    addTask();
+    addTodos();
   }
 });
 
-function addTask() {
+function addTodos() {
   const taskText = inputText.value;
   if (inputTitle.value === "" && inputText.value === "") {
     return;
-  }
-  if (addTaskButton.value === "Edit") {
-    editTodos(editTodo.target.previousElementSibling.innerHTML);
-    editTodo.target.previousElementSibling.innerHTML = taskText;
-    addTaskButton.value = "Add";
-    inputText.value = "";
-    inputTitle.value = "";
-    saveTodosInLocalStorage(taskText);
   } else {
     let toDo = {
       title: inputTitle.value,
       text: inputText.value,
-      value: sortSelect.value,
+      value: prioritySelect.value,
     };
     toDos.push(toDo);
-    createTodos();
+    displayTodos();
     inputTitle.value = "";
     inputText.value = "";
   }
 }
-function updateTodo(e) {
-  if (e.target.closest(".remove-task-button")) {
-    e.target.parentElement.remove();
-    deleteTodos(e.target.parentElement);
+function clickOnButtons(e) {
+  if (e.target.closest(".remove-todos-button")) {
+    let id = e.target.getAttribute("data-id");
+    let todo = toDos[id];
+    let todoIndex = toDos.indexOf(todo);
+    toDos.splice(todoIndex, 1);
+    saveTodosInLocalStorage();
   }
 
-  if (e.target.closest(".edit-task-button")) {
-    inputText.value = e.target.previousElementSibling.innerHTML;
-    inputText.focus();
-    addTaskButton.value = "Edit";
-    editTodo = e;
-    editTodos(e.target.previousElementSibling.innerHTML);
+  if (e.target.closest(".edit-todos-button")) {
+    let id = e.target.getAttribute("data-id");
+    let todo = toDos[id];
+    inputTitle.value = todo.title;
+    inputText.value = todo.text;
+    addTodosButton.innerHTML = "Save";
+
+    addTodosButton.addEventListener("click", function () {
+      let todoIndex = toDos.indexOf(todo);
+      addTodosButton.innerHTML = "Add";
+      saveTodosInLocalStorage();
+    });
   }
+
+  displayTodos();
 }
 
 function saveTodosInLocalStorage() {
   localStorage.setItem("toDos", JSON.stringify(toDos));
 }
 
-function createTodos() {
+function displayTodos() {
+  listContainer.innerHTML = "";
+
   for (let i = 0; i < toDos.length; i++) {
     let div = document.createElement("div");
-    div.classList.add("input");
     let h4 = document.createElement("h4");
     h4.innerHTML = toDos[i].title;
     div.appendChild(h4);
@@ -83,35 +83,48 @@ function createTodos() {
     p.innerHTML = toDos[i].text;
     div.appendChild(p);
     saveTodosInLocalStorage();
-    
-    let editTaskButton = document.createElement("button");
-    editTaskButton.classList.add("edit-task-button");
-    editTaskButton.innerHTML = "Edit";
-    div.appendChild(editTaskButton);
 
-    let removeTaskButton = document.createElement("button");
-    removeTaskButton.classList.add("remove-task-button");
-    removeTaskButton.innerHTML = "Remove";
-    div.appendChild(removeTaskButton);
+    let editTodosButton = document.createElement("button");
+    editTodosButton.classList.add("edit-todos-button");
+    editTodosButton.setAttribute("data-id", i);
+    editTodosButton.innerHTML = "Edit";
+    div.appendChild(editTodosButton);
+
+    let removeTodosButton = document.createElement("button");
+    removeTodosButton.classList.add("remove-todos-button");
+    removeTodosButton.setAttribute("data-id", i);
+    removeTodosButton.innerHTML = "Remove";
+    div.appendChild(removeTodosButton);
 
     listContainer.appendChild(div);
   }
 }
 
-function deleteTodos(todo) {
-  let todoText = todo.children[0].innerHTML;
-  let todoIndex = toDos.indexOf(todoText);
-  toDos.splice(todoIndex, 1);
+function sortSelect() {
+  let selectedValue = sortBySelect.value;
+
+  switch (selectedValue) {
+    case "1":
+      console.log("hallo");
+      toDos.sort((a, b) => a.value - b.value);
+      break;
+    case "2":
+      break;
+    case "3":
+      toDos.sort((a, b) => b.value - a.value);
+      break;
+  }
   saveTodosInLocalStorage();
 }
 
-function editTodos(todo) {
-  toDos = JSON.parse(localStorage.getItem("toDos"));
-  let todoIndex = toDos.indexOf(todo);
-  toDos[todoIndex] = inputText.value;
-  saveTodosInLocalStorage();
-}
+// function editTodos(todo) {
+//   toDos = JSON.parse(localStorage.getItem("toDos"));
+//   inputText.value = todo.text;
+//   inputTitle.value = todo.title;
+//   addTodosButton.innerHTML = "Save";
 
-document.addEventListener("DOMContentLoaded", getTodos);
-addTaskButton.addEventListener("click", addTask);
-listContainer.addEventListener("click", updateTodo);
+//   // let todoIndex = toDos.indexOf(todo);
+//   // console.log(todoIndex)
+//   // toDos[todoIndex] = inputText.value;
+//   saveTodosInLocalStorage();
+// }
